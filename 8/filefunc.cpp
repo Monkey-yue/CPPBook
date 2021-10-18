bbbb4cdf2bcdaf2de5a98583c4226f8c4d95a4eb
Carrybook <houyue900@aliyun.com>
2021-10-18 16:38:55 +0800

    继承入门

--- 8/filefunc.cpp
+++ 8/filefunc.cpp
@@ -15,6 +15,7 @@ int main()
         cout << "Can't open " << fn << ".Bye.\n";
         exit(EXIT_FAILURE);
     }
+    
     double objective;
     cout << "Enter the focal length of your"
             "telescope objective in mm: ";
@@ -29,11 +30,30 @@ int main()
     }
     file_it(fout, objective, eps, LIMIT);
     file_it(cout, objective, eps, LIMIT);
-    cout << "Done\n"
+    cout << "Done\n";
     return 0;
 }
 
 void file_it(ostream & os, double fo, const double fe[], int n)
 {
-    ios_base::fmtflags initial;
+    ios_base::fmtflags initial; // 存储类型
+    initial = os.setf(ios_base::fixed); // 定点表示法模式
+    os.precision(0);
+    os << "Focal length of objective: " << fo << " mm\n";
+    os.setf(ios::showpoint);    // 小数点模式，即使后面为0
+    os.precision(1);
+    
+    os.width(12);   // 下次输出的字段宽度
+    os << "f.l. eyepiece";
+    os.width(15);
+    os << "magnification" << endl;
+
+    for (int i = 0;i < n;i++)
+    {
+        os.width(12);
+        os << fe[i];
+        os.width(15);
+        os << int (fo/fe[i] + 0.5) << endl;
+    }
+    os.setf(initial);
 }
